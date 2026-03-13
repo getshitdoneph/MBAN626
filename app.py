dff9ea2c21c6116ee46c35550e34637e56b656d9
Carlo I <carloisles@gmail.com>
2026-03-13 23:37:34 +0800

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
 # Page config
 st.set_page_config(
     page_title="Telco Customer Churn Dashboard",
